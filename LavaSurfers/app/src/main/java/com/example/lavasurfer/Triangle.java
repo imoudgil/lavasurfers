cd124939b39d94fd60c3bda237a3a6e123df0e23
J1ngta0Z887 <jingtao3@ualberta.ca>
2025-01-29 18:55:42 -0700

    My branch hihihhaha

---
+++ LavaSurfers/app/src/main/java/com/example/lavasurfer/Triangle.java
@@ -0,0 +1,28 @@
+package com.example.lavasurfer;
+
+public class Triangle extends Shape {
+    private double base;
+    private double height;
+
+    public Triangle(double base, double height) {
+        this.base = base;
+        this.height = height;
+    }
+
+    public double getBase() {
+        return base;
+    }
+
+    public void setBase(double base) {
+        this.base = base;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+}
