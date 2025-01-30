bce421111d84c24f12f86091c61eb1e1a21496c0
Chenqi Yang <onionring@Chenqis-MacBook-Air.local>
2025-01-29 19:03:53 -0700

    square

---
+++ LavaSurfers/app/src/main/java/com/example/lavasurfer/Square.java
@@ -0,0 +1,12 @@
+package com.example.lavasurfer;
+
+public class Square extends Shape{
+    private float length;
+
+    public Square(float length){
+        this.length = length;
+    }
+    private float getLength(){
+        return this.length;
+    }
+}
