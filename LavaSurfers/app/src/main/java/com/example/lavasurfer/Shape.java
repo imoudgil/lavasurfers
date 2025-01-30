cf919fd716b8bbcb440e9e69e03509e21dcf14c6
Anrajsinghdhanju <anraj@ualberta.ca>
2025-01-29 18:27:36 -0700

    Shape

---
+++ LavaSurfers/app/src/main/java/com/example/lavasurfer/Shape.java
@@ -0,0 +1,22 @@
+package com.example.lavasurfer;
+
+public class Shape {
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    private int x;
+    private int y;
+}
