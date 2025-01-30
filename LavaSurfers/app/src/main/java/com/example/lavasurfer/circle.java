54acaf49a7bd4d5619f50036671c1ec2f66ccd72
Anrajsinghdhanju <anraj@ualberta.ca>
2025-01-29 18:52:28 -0700

    circle

---
+++ LavaSurfers/app/src/main/java/com/example/lavasurfer/circle.java
@@ -0,0 +1,14 @@
+package com.example.lavasurfer;
+
+public class circle extends Shape{
+    public int getRadius() {
+        return radius;
+    }
+
+    public void setRadius(int radius) {
+        this.radius = radius;
+    }
+
+    private int radius;
+
+}
